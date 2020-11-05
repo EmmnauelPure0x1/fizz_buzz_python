3e01021d5454f644245cf3ffe51bf21323f1aead
EmmanuelPure0x1 <eadejobi@spartaglobal.com>
2020-11-05 18:43:30 +0000

    Working Instantiation

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,19 @@
-for number in range(1,101):
-    if number % 3 == 0 and number % 5 == 0:
-        print("fizzBuzz")
-    elif number % 3 == 0:
-        print("fizz")
-    elif number % 5 == 0:
-        print("buzz")
-    else:
-        print(number)
+class FizzBuzz:
+    def listed(self):
+        list_num = []
+        for number in range(1, 101):
+            if number % 3 == 0 and number % 5 == 0:
+                list_num.append("fizzbuzz")
+            elif number % 3 == 0:
+                list_num.append("fizz")
+            elif number % 5 == 0:
+                list_num.append("buzz")
+            else:
+                list_num.append(number)
+        return(list_num)
+
+obj = FizzBuzz()
+# print(obj.listed())
+
+for i in obj.listed():
+    print(i)
